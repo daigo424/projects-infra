0c6d368e3d2adf2b92779d44a8a66bbbba7d8048
daigo424 <ishii.daigo@gmail.com>
2026-04-05 15:54:50 +0900

    feat: change permission policy for sso login of every project

--- platform/access/main.tf
+++ platform/access/main.tf
@@ -14,23 +14,23 @@ locals {
   }
 }
 
-resource "aws_ssoadmin_permission_set" "developer" {
+resource "aws_ssoadmin_permission_set" "admin" {
   instance_arn     = var.identity_center_instance_arn
   name             = var.permission_set_name
   session_duration = "PT4H"
 }
 
-resource "aws_ssoadmin_managed_policy_attachment" "poweruser" {
+resource "aws_ssoadmin_managed_policy_attachment" "admin" {
   instance_arn       = var.identity_center_instance_arn
-  permission_set_arn = aws_ssoadmin_permission_set.developer.arn
-  managed_policy_arn = "arn:aws:iam::aws:policy/PowerUserAccess"
+  permission_set_arn = aws_ssoadmin_permission_set.admin.arn
+  managed_policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
 resource "aws_ssoadmin_account_assignment" "project_access" {
   for_each = local.project_configs
 
   instance_arn       = var.identity_center_instance_arn
-  permission_set_arn = aws_ssoadmin_permission_set.developer.arn
+  permission_set_arn = aws_ssoadmin_permission_set.admin.arn
 
   principal_id   = var.principal_group_id
   principal_type = "GROUP"
--- platform/access/terraform.auto.tfvars
+++ platform/access/terraform.auto.tfvars
@@ -1,4 +1,4 @@
 aws_region                   = "ap-northeast-1"
 identity_center_instance_arn = "arn:aws:sso:::instance/ssoins-7758e77521cb9259"
 principal_group_id           = "b7145a58-20b1-7053-e445-328654630de7"
-permission_set_name          = "DeveloperAccess"
+permission_set_name          = "AdministratorAccess"
